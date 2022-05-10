3dbd0ea51cce46a6c35a76d95199d342a496546a
Claudia Hill <hillc255@yahoo.com>
2022-05-10 04:58:15 -0400

    docstyle added

--- write.py
+++ write.py
@@ -15,7 +15,6 @@ from helpers import datetime_to_str
 
 
 def write_to_csv(results, filename, delimiter=","):
-
     """Write an iterable of `CloseApproach` objects to a CSV file.
 
     The precise output specification is in `README.md`. Roughly, each
@@ -53,8 +52,7 @@ def write_to_csv(results, filename, delimiter=","):
             writer.writerow(row_dict)
 
 
-def write_to_json(results, filename):
-
+def write_to_json(results, filename):  
     """Write an iterable of `CloseApproach` objects to a JSON file.
     The precise output specification is in `README.md`. Roughly, the output
     is a list containing dictionaries, each mapping `CloseApproach` attributes
@@ -63,9 +61,8 @@ def write_to_json(results, filename):
 
     :param results: An iterable of `CloseApproach` objects.
     :param filename: A Path-like object pointing to where the data should be
-                     saved.
+    saved.
     """
-
     # opening file to where data should be saved
     with open(filename, 'w') as f:
         # list to capture iteration
