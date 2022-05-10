fe8aaa356f94ab1b7c76c18e7fe80f259e08aad3
Claudia Hill <hillc255@yahoo.com>
2022-05-10 06:26:23 -0400

    update pycodestyle

--- write.py
+++ write.py
@@ -52,14 +52,14 @@ def write_to_csv(results, filename, delimiter=","):
             writer.writerow(row_dict)
 
 
-def write_to_json(results, filename):  
+def write_to_json(results, filename):
     """Write an iterable of `CloseApproach` objects to a JSON file.
-    
+
     The precise output specification is in `README.md`. Roughly, the output
     is a list containing dictionaries, each mapping `CloseApproach` attributes
     to their values and the 'neo' key mapping to a dictionary of the associated
     NEO's attributes.
-    
+
     :param results: An iterable of `CloseApproach` objects.
     :param filename: A Path-like object pointing to where the data should be
     saved.
